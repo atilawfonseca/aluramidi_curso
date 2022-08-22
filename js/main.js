cf9ab27d5a088fa18e5465d5321d7f057b12bd50
atilawfonseca <eng.atila.sete@gmail.com>
2022-08-22 13:40:23 -0300

    finalizado projeto com som de instrumento

--- js/main.js
+++ js/main.js
@@ -1,21 +1,42 @@
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
+function tocaSom(ElementoAudio){
+
+    const audio = document.querySelector(ElementoAudio);
+    //audio = se o elemento não for null
+    if(audio && audio.localName === 'audio')
+    {
+        audio.play();
+    }
+    else {
+        console.log("Elemento nulo ou não é um elemento de audio");
+    }
+    
+
+    
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0; 
-
-while(contador < listaDeTeclas.length){
-
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador]; 
-
-    //template string 
-    const instrumento = `#som_${tecla.classList[1]}`;
+    const instrumento = `#som_${tecla.classList[1]}`; //template string
 
     tecla.onclick = function () {
         tocaSom(instrumento);
     }
-    //console.log(contador);
-    contador++; 
+    tecla.onkeydown = function (evento) {
+        console.log(evento.code == 'Space' || evento.code == 'Enter');
+        if(evento.code == 'Space' || evento.code == 'Enter')
+        { 
+            tecla.classList.add['ativa'];
+        }
+        
+    }
+    //comparador === vai verificar se o valor e o tipo são iguais
+    // quanto o operador == vai comparar apenas o valor. 
+    tecla.onkeyup = function (evento) {
+        if(evento.code === 'Space' || evento.code === 'Enter')
+        {
+            tecla.classList.remove['ativa'];
+        }
+    }
 }
