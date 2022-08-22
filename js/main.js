738d1e7c5c72132d06dfd1af281ab7c02a3312d3
atilawfonseca <eng.atila.sete@gmail.com>
2022-08-22 09:43:02 -0300

    javascript atualizado

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-function teclaSomPom (){
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
 
-document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0; 
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador]; 
+
+    //template string 
+    const instrumento = `#som_${tecla.classList[1]}`;
+
+    tecla.onclick = function () {
+        tocaSom(instrumento);
+    }
+    //console.log(contador);
+    contador++; 
+}
